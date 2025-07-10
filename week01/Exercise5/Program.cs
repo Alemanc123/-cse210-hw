47b0834e68f433b024a0b237a0a377add40cff82
Alemanc123 <theminiband@hotmail.com>
2025-07-09 22:45:13 -0600

    Commit

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,55 @@
 using System;
-
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayMessage();
+
+        string name = AskForName();
+        int number = AskForNumber();
+
+        int squaredNumber = squareNumber(number);
+
+        Result(name, squaredNumber);
+
     }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    static string AskForName()
+
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+
+    }
+
+    static int AskForNumber()
+
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+
+    }
+
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void Result(string name, int square)
+
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
 }
